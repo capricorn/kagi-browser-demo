217266c4eefee085512478a5883a6d95b71eb64b
capricorn <8250820+capricorn@users.noreply.github.com>
2024-02-27 10:44:00 -0700

    Set user agent to firefox for firefox extensions

--- BrowserDemo/WebViewController.swift
+++ BrowserDemo/WebViewController.swift
@@ -15,13 +15,14 @@ class WebViewController: UIViewController, WKUIDelegate {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:70.0) Gecko/20100101 Firefox/70.0"
         view = webView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myURL = URL(string:"https://www.kagi.com")
+        let myURL = URL(string:"https://addons.mozilla.org/en-US/firefox/addon/top-sites-button/")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
     }
