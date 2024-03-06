97a209d237426c9bdab038f14b13b7ab43347a0e
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-06 14:48:44 -0700

    Adjust searchbar constraints given keyboard show/hide

--- BrowserDemo/ViewController.swift
+++ BrowserDemo/ViewController.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 import WebKit
+import Combine
 
 class ViewController: UIViewController {
     private var label: UITextView!
+    private var webView: UIView!
+    private var toolbarView: UIView!
     private var toolbarDelegate: ToolbarDelegate!
+    private var keyboardOpenSubscriber: AnyCancellable? = nil
+    private var keyboardCloseSubscriber: AnyCancellable? = nil
+    
+    private var keyboardYOffset: CGFloat = 0
+    private var toolbarBottomConstraint: NSLayoutConstraint!
     
     private class ToolbarDelegate: BrowserToolbarDelegate {
         let webViewController: WebViewController
@@ -102,33 +110,59 @@ class ViewController: UIViewController {
             }
         }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let safeArea = view.safeAreaLayoutGuide
+    
+    func setupConstraints() {
+        let safeArea = view.layoutMarginsGuide
         
-        let webViewController = WebViewController()
-        let webView = webViewController.view!
-        
-        toolbarDelegate = ToolbarDelegate(webViewController: webViewController)
-        
-        let toolbar = BrowserToolbarViewController(delegate: toolbarDelegate)
-        self.addChild(toolbar)
-        self.view.addSubview(toolbar.view)
-        
-        toolbar.view.translatesAutoresizingMaskIntoConstraints = false
-        toolbar.view.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
-        toolbar.view.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
-        toolbar.view.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
-        toolbar.view.heightAnchor.constraint(equalToConstant: 32).isActive = true
-        
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        self.addChild(webViewController)
-        self.view.addSubview(webView)
+        toolbarBottomConstraint = toolbarView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -self.keyboardYOffset)
+        toolbarBottomConstraint.isActive = true
+        toolbarView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        toolbarView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+        toolbarView.heightAnchor.constraint(equalToConstant: 32).isActive = true
         
         webView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
         webView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
         webView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
-        webView.bottomAnchor.constraint(equalTo: toolbar.view.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: toolbarView.topAnchor).isActive = true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.keyboardOpenSubscriber = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification).sink { [weak self] notification in
+            guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+                return
+            }
+            
+            self?.keyboardYOffset = keyboardFrame.height
+            UIView.animate(withDuration: 1/4, animations: { [weak self] in
+                self?.toolbarBottomConstraint.constant = -(self?.keyboardYOffset ?? 0)
+                self?.view.layoutIfNeeded()
+            })
+        }
+        
+        self.keyboardCloseSubscriber = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification).sink { [weak self] _ in
+            UIView.animate(withDuration: 1/4, animations: { [weak self] in
+                self?.toolbarBottomConstraint.constant = 0
+                self?.view.layoutIfNeeded()
+            })
+        }
+
+        let webViewController = WebViewController()
+        webView = webViewController.view!
+        
+        toolbarDelegate = ToolbarDelegate(webViewController: webViewController)
+        
+        let toolbar = BrowserToolbarViewController(delegate: toolbarDelegate)
+        toolbarView = toolbar.view!
+        self.addChild(toolbar)
+        self.view.addSubview(toolbar.view)
+        
+        toolbarView.translatesAutoresizingMaskIntoConstraints = false
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        self.addChild(webViewController)
+        self.view.addSubview(webView)
+        
+        setupConstraints()
     }
 }
