ef0722b55388b0e2693146b2093a114af89b0c6d
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-07 20:16:34 -0700

    Move url extension tests to separate file

--- BrowserDemoTests/BrowserDemoTests.swift
+++ BrowserDemoTests/BrowserDemoTests.swift
@@ -10,23 +10,5 @@ import XCTest
 
 final class BrowserDemoTests: XCTestCase {
     override func setUpWithError() throws {}
-
     override func tearDownWithError() throws {}
-    
-    func testURLRelativePath() throws {
-        let url = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/popup/panel.html")!
-        let baseURL = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/")!
-        
-        XCTAssert(url.relativePath(from: baseURL)?.absoluteString == "popup/panel.html")
-    }
-    
-    func testSameBasePath() throws {
-        let url = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/popup/panel.html")!
-        let baseURL = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/")!
-        
-        let absoluteURL = URL(string: "extension:///popup/popup.html")!
-        
-        XCTAssert(url.sameBasePath(as: baseURL))
-        XCTAssertFalse(url.sameBasePath(as: absoluteURL))
-    }
 }
---
+++ BrowserDemoTests/URLExtensionTests.swift
@@ -0,0 +1,33 @@
+//
+//  URLExtensionTests.swift
+//  BrowserDemoTests
+//
+//  Created by Collin Palmer on 3/7/24.
+//
+
+import XCTest
+@testable import BrowserDemo
+
+final class URLExtensionTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testURLRelativePath() throws {
+        let url = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/popup/panel.html")!
+        let baseURL = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/")!
+        
+        XCTAssert(url.relativePath(from: baseURL)?.absoluteString == "popup/panel.html")
+    }
+    
+    func testSameBasePath() throws {
+        let url = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/popup/panel.html")!
+        let baseURL = URL(string: "extension:///Users/collin/Library/Developer/CoreSimulator/Devices/4C7E0ADB-5A09-43BA-9C30-EA41E4D26ADE/data/Containers/Bundle/Application/BED4C5FF-2B49-4385-B7F4-319492AC6EAC/BrowserDemo.app/top_sites_button-1.5/")!
+        
+        let absoluteURL = URL(string: "extension:///popup/popup.html")!
+        
+        XCTAssert(url.sameBasePath(as: baseURL))
+        XCTAssertFalse(url.sameBasePath(as: absoluteURL))
+    }
+}
